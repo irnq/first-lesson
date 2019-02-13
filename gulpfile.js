a3fa0befe9512f0f825af5a72cdbc22ef21d9cb1
irnq <47535562+irnq@users.noreply.github.com>
2019-02-13 22:14:30 +0400

    new branch for home or idk

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require ('browser-sync');
 
 gulp.task('sass', function() {
     return gulp.src('app/sass/**/*.sass')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
 });
 
-
+gulp.task('browser-sync', function(){
+    browserSync({
+        server:{
+            baseDir: 'app'
+        },
+        notify: false
+    })
+})
 
 gulp.task('watch', function() {
     gulp.watch('app/sass/*.sass', gulp.series('sass'));
 });
+
+gulp.task('default', gulp.parallel('watch','browser-sync','sass'))
+
