8ac740b4349e5a3d20bc3c38120e3260b55e0585
irnq-lt <SERVER\mail>
2019-02-13 15:38:53 +0400

    gulpfile updated, empty js added

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync');
-
-gulp.task('sass', function() {
-    return gulp.src('app/sass/**/*.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream:true}))
-});
-
+    
 gulp.task('browser-sync', function(){
     browserSync({
         server:{
@@ -16,11 +9,26 @@ gulp.task('browser-sync', function(){
         },
         notify: false
     })
-})
-
-gulp.task('watch', function() {
-    gulp.watch('app/sass/*.sass', gulp.series('sass'));
 });
 
-gulp.task('default',gulp.parallel('sass','watch','browser-sync'), function(){
-})
+gulp.task('bws', function() {
+    return gulp.src('app/**/*.html')
+    .pipe(browserSync.reload({stream:true}));
+    return gulp.src('app/js/*.js')
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/sass/**/*.sass')
+    .pipe(sass({outputStyle: 'expanded'}).on('error',sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/*.html', gulp.series('bws'));
+    gulp.watch('app/sass/*.sass', gulp.series('sass'));
+    gulp.watch('app/js/**/*.js', gulp.series('bws'));
+});
+
+gulp.task('default',gulp.parallel('sass','watch','browser-sync' ))
